2b4656264d46c1d2a1ac69e3782f0de0511cb373
Emmanuel Asante <prof_asante@DESKTOP-5588AI2.localdomain>
2023-03-28 10:19:35 -1200

    Printf project

--- _printf.c
+++ _printf.c
@@ -1,15 +1,56 @@
 #include "main.h"
 
 /**
-   * _printf - produces output according to a format
-    * @format: character string
-     * Return: number of characters printed
-      */
+  * cases - cases c, s & % for _printf
+  * @format: character string
+  * @i: index
+  * @args: arguments
+  * Return: number of characters printed
+  */
+int cases(const char *format, int i, va_list args)
+{
+	int j = 0, count = 0;
+	char *str;
+
+	switch (format[i])
+	{
+		case 'c':
+			_putchar(va_arg(args, int));
+			count++;
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+				str = "(null)";
+			for (j = 0; str[j] != '\0'; j++)
+			{
+				_putchar(str[j]);
+				count++;
+			}
+			break;
+		case '%':
+			_putchar('%');
+			count++;
+			break;
+		default:
+			_putchar('%');
+			_putchar(format[i]);
+			count += 2;
+			break;
+	}
+
+	return (count);
+}
+
+/**
+  * _printf - produces output according to a format
+  * @format: character string
+  * Return: number of characters printed
+  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, count = 0;
+	int i = 0, count = 0;
 	va_list args;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
@@ -19,32 +60,7 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
-			{
-				case 'c':
-					_putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					if (str == NULL)
-						str = "(null)";
-					for (j = 0; str[j] != '\0'; j++)
-					{
-						_putchar(str[j]);
-						count++;
-					}
-					break;
-				case '%':
-					_putchar('%');
-					count++;
-					break;
-				default:
-					_putchar('%');
-					_putchar(format[i]);
-					count += 2;
-					break;
-			}
+			count += cases(format, i, args);
 		}
 		else
 		{
