b4f790574da4dcd82274f7e855f5cb86bef82a6d
Emmanuel Asante <prof_asante@DESKTOP-5588AI2.localdomain>
2023-03-27 21:31:43 -1200

    function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+   * _printf - produces output according to a format
+    * @format: character string
+     * Return: number of characters printed
+      */
+int _printf(const char *format, ...)
+{
+	int i = 0, j = 0, count = 0;
+	va_list args;
+	char *str;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(null)";
+					for (j = 0; str[j] != '\0'; j++)
+					{
+						_putchar(str[j]);
+						count++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <vadefs.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
