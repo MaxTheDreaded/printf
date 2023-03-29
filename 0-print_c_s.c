3a2a6cfc25bf57dab31905407b167fa556cad1a9
Asante <asantemma7@gmail.com>
2023-03-28 16:33:40 -1200

    Printf Task

---
+++ 0-print_c_s.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_char - function that prints a character
+ * @args: arguments
+ * Return: number of characters printed
+ */
+
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - function that prints a string
+ * @args: arguments
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	int i = 0;
+	char *s = va_arg(args, char *);
+	if (s == NULL)
+		s = "(null)";
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_percent - function that prints a percent
+ * @args: arguments
+ * Return: number of characters printed
+ */
+int print_percent()
+{
+	_putchar('%');
+	return (1);
+}
---
+++ _print-1.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * cases - function that selects the correct function to perform the operation
+ * @format: format
+ * @print: struct
+ * @args: arguments
+ * Return: number of characters printed
+ */
+int cases(const char *format, format_t *print, va_list args)
+{
+	int i = 0, j = 0, count = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (print[j].f != NULL)
+			{
+				if (format[i + 1] == print[j].f[0])
+				{
+					count += print[j].func(args);
+					break;
+				}
+				j++;
+			}
+			if (print[j].f == NULL)
+			{
+				if (format[i + 1] == '%')
+				{
+					_putchar('%');
+					count++;
+				}
+				else
+				{
+					_putchar('%');
+					_putchar(format[i + 1]);
+					count += 2;
+				}
+			}
+			i += 2;
+		}
+		else
+		{
+			_putchar(format[i]);
+			i++;
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: format
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	format_t print[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_int},
+		{"i", print_int},
+		{"b", print_binary},
+		{"u", print_unsigned},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{NULL, NULL}
+	};
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	count = cases(format, print, args);
+	va_end(args);
+	return (count);
+}
